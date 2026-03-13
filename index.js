15bf6e8dfeae4083097c5f73a9e4eb70bfab316d
Bastian <salve.sebast@gmail.com>
2026-03-13 13:44:40 -0500

    feat: agregar validacion y middleware

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
+const env = require('dotenv').config();
+const port = process.env.PORT || 3000;
+const pass = process.env.API_PASSWORD;
+
 
 require('./db'); // ← AGREGA ESTA LÍNEA
 
-app.use(express.json());
 
 const Estudiantes = require('./routers/Estudiantes');
 const Profesores = require('./routers/Profesores');
 const materias = require('./routers/Materias');
 const notas = require('./routers/Notas');
+app.use(express.json());
+
+
+app.use((req, res, next) =>{
+    const apiKey = req.headers['password'];
+    if (!apiKey) return res.status(401).json({ success: false, message: 'API key requerida' });
+    if (apiKey !== process.env.API_PASSWORD) {
+        return res.status(403).json({ success: false, message: 'Password incorrecta' });
+    }
+    next();
+});
 
 app.use('/estudiantes', Estudiantes);
 app.use('/profesores', Profesores);
